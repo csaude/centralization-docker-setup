aa35b577039f19eb2698ed67d57cbaabc103dc1c
JPboane <jpboane@gmail.com>
2023-02-22 12:02:35 +0200

    Added SSL acceptor on artemis broker template

--- dbsync-central/release_stuff/scripts/release_info.sh
+++ dbsync-central/release_stuff/scripts/release_info.sh
@@ -2,7 +2,7 @@
 # EIP RELEASES INFO
 #
 export RELEASE_NAME="EIP release 3.0.0.0"
-export RELEASE_DATE="2022-12-05 9:00:00"
-export RELEASE_DESC="New release with several improvments and new feactures"
+export RELEASE_DATE="2023-02-22 11:30:00"
+export RELEASE_DESC="Testing release"
 
-export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v3.0.1.0/openmrs-eip-app-1.1.jar"
+export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/testing/openmrs-eip-app-2.0-SNAPSHOT.jar"
