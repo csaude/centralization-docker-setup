179ae7d777d5c52b9e5ec5b2caf98ae93ec26921
JPboane <jpboane@gmail.com>
2024-12-11 10:04:59 +0200

    Fix Compatibility issues With MySQL 8.4.2 and OpenMRS Platform 2.6.9

--- openmrs-central/setenv.sh
+++ openmrs-central/setenv.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 export JAVA_OPTS="-Dorg.openmrs.module.debezium.snapshotOnly"
+export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Djava.awt.headless=true -XX:+UseConcMarkSweepGC -Xms1536m -Xmx6144m -XX:PermSize=768m -XX:MaxPermSize=1024m -Duser.timezone=Africa/Maputo"
+export OPENMRS_RUNTIME_PROPERTIES_FILE='/usr/local/tomcat/openmrs-runtime.properties'
