47ecb44edb57496a110c34b8ca4fdc4a439fb8df
JPboane <jpboane@gmail.com>
2025-09-26 14:28:08 +0200

    Prepare September/2025 Release

--- dbsync-central/release_stuff/scripts/release_info.sh
+++ dbsync-central/release_stuff/scripts/release_info.sh
@@ -2,16 +2,10 @@
 # EIP RELEASES INFO
 #
 
-HOME_DIR="/home/eip"
-RELEASE_BASE_DIR="$HOME_DIR/openmrs-eip-docker"
-RELEASE_DIR="$RELEASE_BASE_DIR/release_stuff"
-RELEASE_SCRIPTS_DIR="$RELEASE_DIR/scripts"
-
-export RELEASE_VERSION_TAG="V11.0.0"
+export RELEASE_VERSION_TAG="1.9.2"
 export RELEASE_NAME="EIP-Release-$RELEASE_VERSION_TAG"
-export RELEASE_DATE="2025-05-20 12:00:00"
-export RELEASE_DESC="DBSync v1.9x, upgrade of OpenMRS to a new release and accommodation with DBSync"
+export RELEASE_DATE="2025-09-26 12:00:00"
+export RELEASE_DESC="DBSync v1.9.2"
 
-export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/csaude/openmrs-eip-docker/releases/download/${RELEASE_VERSION_TAG}/openmrs-eip-app-1.9.1.jar"
-export EPTS_ETL_API_RELEASE_URL="https://github.com/csaude/openmrs-eip-docker/releases/download/${RELEASE_VERSION_TAG}/epts-etl-api-1.0.jar"
-export CENTRALIZATION_FEATURES_MANAGER_RELEASE_URL="https://github.com/csaude/openmrs-eip-docker/releases/download/${RELEASE_VERSION_TAG}/centralization-features-manager-1.0.jar"
+export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/csaude/openmrs-eip/releases/download/${RELEASE_VERSION_TAG}/openmrs-eip-app-1.9.2.jar"
+export CENTRALIZATION_FEATURES_MANAGER_RELEASE_URL="https://github.com/csaude/openmrs-eip/releases/download/${RELEASE_VERSION_TAG}/centralization-features-manager-1.0.jar"
